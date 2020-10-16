7916ab5b511c0115c1641066ddbe99ccaa2efdf2
BigBigor <igorapm.pi@gmail.com>
2020-10-16 15:21:01 +0200

    added victory plugin and fanned shots

---
+++ Source/CB_Reborn/Private/CB_Reborn.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CB_Reborn.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CB_Reborn, "CB_Reborn" );
